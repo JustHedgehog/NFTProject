b6be71a112115f605d6ea3569dd39164ccf652e5
Sebastian Szelest <seba3444gaming@gmail.com>
2021-12-01 13:26:38 +0100

    35:35

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -2,14 +2,26 @@ pragma solidity ^0.8.10;
 
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "base64-sol/base64.sol";
 
 contract SVGNFT is ERC721URIStorage{
+    uint256 public tokenCounter;
     constructor() ERC721 ("SVG NFT", "svgNFT"){
-
+        tokenCounter= 0;
     }
 
     function create(string memory svg) public{
-        
+        _safeMint(msg.sender, tokenCounter);
+        string memory imageURI = svgToImageURI(svg);
+        tokenCounter+= 1;
+
+    }
+
+    function svgToImageURI(string memory svg) public pure returns(string memory){
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        string memory imageURI = string(abi.encodePacked(baseURL,svgBase64Encoded));
+        return imageURI;
     }
 
 }
