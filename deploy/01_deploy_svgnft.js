c449c75fa96ccf6ac615518cad08063572e2f5f2
Frixenos <frixenos@gmail.com>
2022-01-12 19:06:40 +0100

    deploy v2

---
+++ deploy/01_deploy_svgnft.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+let {networkConfig} = require('../helper-hardhat-config')
+
+
+module.exports = async({
+    getNamedAccounts,
+    deployments,
+    getChainId
+}) => {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+    const chainId = await getChainId()
+
+    log("-------------------------------------")
+    const SVGNFT = await deploy("SVGNFT", {
+        from: deployer,
+        log: true
+    })
+    log(`You have deployed an NFT contract to ${SVGNFT.address}`)
+    let filepath = "./img/randomig.svg"
+    let svg = fs.readFileSync(filepath, {encoding: "utf8"})
+
+    const svgNFTContract = await ethers.getContractFactory("SVGNFT")
+    const accounts = await hrtime.ethers.getSigners()
+    const signer = accounts[0]
+    const cvgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer)
+    const networkName = networkConfig[chainId]['name']
+    log(`Verify with: \n npx hardhat verify --network ${networkName} ${svgNFT.address}`)
+
+    let transactionResponse = await svgNFT.create(svg)
+    let receipt = await transactionResponse.wait(1)
+    log(`You've made an NFT!`)
+    log(`You can view the tokenURI here ${await svgNFT.tokenURI(0)}`)
+}
