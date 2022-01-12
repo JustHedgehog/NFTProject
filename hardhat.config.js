20957086bbe9ab540ba5117e447593e84098a49a
Sebastian Szelest <seba3444gaming@gmail.com>
2022-01-13 00:44:56 +0100

    Hardhat ogarniety

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,11 +25,29 @@ module.exports = {
     // rinkeby: {
     //   accounts: { }
     // }
-  }
-  solidity: "0.8.10",
+  },
   namedAccounts: {
     deployer: {
       default: 0
     }
+  },
+  solidity: {
+    compilers: [
+        {
+          version: "0.8.10"
+        },
+        {
+            version: "0.8.0"
+        },
+        {
+            version: "0.7.0"
+        },
+        {
+            version: "0.6.6"
+        },
+        {
+            version: "0.4.24"
+        }
+    ]
   }
 };
