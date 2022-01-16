695b402f1a0c2275421cdb04b4d0b88033adaec5
Sebastian Szelest <seba3444gaming@gmail.com>
2022-01-16 19:59:45 +0100

    Poprawki które juz tworza nft

--- deploy/01_deploy_svgnft.js
+++ deploy/01_deploy_svgnft.js
@@ -17,13 +17,13 @@ module.exports = async({
         log: true
     })
     log(`You have deployed an NFT contract to ${SVGNFT.address}`)
-    let filepath = "./img/randomig.svg"
+    let filepath = "./img/randomimg.svg"
     let svg = fs.readFileSync(filepath, {encoding: "utf8"})
 
     const svgNFTContract = await ethers.getContractFactory("SVGNFT")
-    const accounts = await hrtime.ethers.getSigners()
+    const accounts = await hre.ethers.getSigners()
     const signer = accounts[0]
-    const cvgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer)
+    const svgNFT = new ethers.Contract(SVGNFT.address, svgNFTContract.interface, signer)
     const networkName = networkConfig[chainId]['name']
     log(`Verify with: \n npx hardhat verify --network ${networkName} ${svgNFT.address}`)
 
